45f4f377bd21f927886eb30810f0b1677e54c277
joegos1 <jorgthebestgamer@gmail.com>
2025-01-13 13:16:53 +0100

    update beretta m1934
    foto's toegevoegd en beschrijving

--- js/weapons.js
+++ js/weapons.js
@@ -1,19 +1,20 @@
 const weaponsData = [
     {
         id: 1,
-        naam: "Longsword",
-        type: "zwaard",
-        periode: "middeleeuwen",
-        jaar: "1100-1500",
-        beschrijving: "Het longsword was een veelzijdig zwaard dat zowel met één als twee handen gebruikt kon worden. Het was het standaard wapen van veel Europese ridders.",
+        naam: "Beretta",
+        type: "Pistool",
+        periode: "Tweede Wereldoorlog",
+        jaar: "1934-1991",
+        beschrijving: "De Beretta M1934 is een Italiaans pistool dat in 1934 werd geïntroduceerd. Het was het standaarddienstwapen van het Koninklijk Italiaans Leger tijdens de Tweede Wereldoorlog. Het pistool vuurt .380 ACP patronen af en heeft een magazijncapaciteit van 7+1. Door zijn compacte ontwerp, betrouwbaarheid en gebruiksgemak werd het zeer gewaardeerd door militairen. Het pistool bleef in gebruik tot ver na de oorlog en is vandaag de dag geliefd bij verzamelaars vanwege zijn historische betekenis.",
         details: {
-            lengte: "100-140cm",
-            gewicht: "1.1-1.8kg",
+            caliber: ".380 ACP",
+            gewicht: "660g",
             materiaal: "Staal",
             gebruik: "Militair en civiel"
         },
-        image: "/api/placeholder/400/300"
-    },
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Beretta_34_%286825664724%29.jpg/450px-Beretta_34_%286825664724%29.jpg"
+        },
+        
     {
         id: 2,
         naam: "English Longbow",

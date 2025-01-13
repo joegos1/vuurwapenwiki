e94ee4eb15be5be0d0643ae80a1486781b4f39f4
joegos1 <jorgthebestgamer@gmail.com>
2025-01-13 11:08:42 +0100

    weapons.js
    wapens.js toegevoegd

---
+++ Javascript/weapons.js
@@ -0,0 +1,47 @@
+const weaponsData = [
+    {
+        id: 1,
+        naam: "Longsword",
+        type: "zwaard",
+        periode: "middeleeuwen",
+        jaar: "1100-1500",
+        beschrijving: "Het longsword was een veelzijdig zwaard dat zowel met één als twee handen gebruikt kon worden. Het was het standaard wapen van veel Europese ridders.",
+        details: {
+            lengte: "100-140cm",
+            gewicht: "1.1-1.8kg",
+            materiaal: "Staal",
+            gebruik: "Militair en civiel"
+        },
+        image: "/api/placeholder/400/300"
+    },
+    {
+        id: 2,
+        naam: "English Longbow",
+        type: "boog",
+        periode: "middeleeuwen",
+        jaar: "1200-1500",
+        beschrijving: "De English Longbow was een krachtig wapen dat bekend staat om zijn gebruik in middeleeuwse veldslagen zoals de Slag bij Agincourt.",
+        details: {
+            lengte: "180-200cm",
+            trekkracht: "80-150 pond",
+            materiaal: "Taxushout",
+            gebruik: "Militair"
+        },
+        image: "/api/placeholder/400/300"
+    },
+    {
+        id: 3,
+        naam: "Arquebus",
+        type: "vuurwapen",
+        periode: "renaissance",
+        jaar: "1400-1600",
+        beschrijving: "De arquebus was een van de eerste draagbare vuurwapens en markeerde het begin van een nieuwe era in oorlogsvoering.",
+        details: {
+            lengte: "90-120cm",
+            kaliber: "12-20mm",
+            materiaal: "IJzer en hout",
+            gebruik: "Militair"
+        },
+        image: "/api/placeholder/400/300"
+    }
+];
